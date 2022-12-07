21cec64436ed10bf7bd469faa25578c5c3267c14
Iheb <iheb.draouil@outlook.com>
2022-12-07 19:49:53 +0100

    refactoring

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -4,58 +4,61 @@ const sut = new PassengersSeatsArrangementCalculator();
 
 test('test-1', () => {
 
-    const expected = {
-        1: { blockIndex: 0, row: 0, column: 2 },
-        2: { blockIndex: 1, row: 0, column: 0 },
-        3: { blockIndex: 1, row: 0, column: 3 },
-        4: { blockIndex: 2, row: 0, column: 0 },
-        5: { blockIndex: 2, row: 0, column: 1 },
-        6: { blockIndex: 3, row: 0, column: 0 },
-        7: { blockIndex: 0, row: 1, column: 2 },
-        8: { blockIndex: 1, row: 1, column: 0 },
-        9: { blockIndex: 1, row: 1, column: 3 },
-        10: { blockIndex: 2, row: 1, column: 0 },
-        11: { blockIndex: 2, row: 1, column: 1 },
-        12: { blockIndex: 3, row: 1, column: 0 },
-        13: { blockIndex: 1, row: 2, column: 0 },
-        14: { blockIndex: 1, row: 2, column: 3 },
-        15: { blockIndex: 2, row: 2, column: 0 },
-        16: { blockIndex: 2, row: 2, column: 1 },
-        17: { blockIndex: 3, row: 2, column: 0 },
-        18: { blockIndex: 3, row: 3, column: 0 },
-        19: { blockIndex: 0, row: 0, column: 0 },
-        20: { blockIndex: 3, row: 0, column: 2 },
-        21: { blockIndex: 0, row: 1, column: 0 },
-        22: { blockIndex: 3, row: 1, column: 2 },
-        23: { blockIndex: 3, row: 2, column: 2 },
-        24: { blockIndex: 3, row: 3, column: 2 },
-        25: { blockIndex: 0, row: 0, column: 1 },
-        26: { blockIndex: 1, row: 0, column: 1 },
-        27: { blockIndex: 1, row: 0, column: 2 },
-        28: { blockIndex: 3, row: 0, column: 1 },
-        29: { blockIndex: 0, row: 1, column: 1 },
-        30: { blockIndex: 1, row: 1, column: 1 },
-    };
+    const expected = [
+        { blockIndex: 0, row: 0, column: 2 },
+        { blockIndex: 1, row: 0, column: 0 },
+        { blockIndex: 1, row: 0, column: 3 },
+        { blockIndex: 2, row: 0, column: 0 },
+        { blockIndex: 2, row: 0, column: 1 },
+        { blockIndex: 3, row: 0, column: 0 },
+        { blockIndex: 0, row: 1, column: 2 },
+        { blockIndex: 1, row: 1, column: 0 },
+        { blockIndex: 1, row: 1, column: 3 },
+        { blockIndex: 2, row: 1, column: 0 },
+        { blockIndex: 2, row: 1, column: 1 },
+        { blockIndex: 3, row: 1, column: 0 },
+        { blockIndex: 1, row: 2, column: 0 },
+        { blockIndex: 1, row: 2, column: 3 },
+        { blockIndex: 2, row: 2, column: 0 },
+        { blockIndex: 2, row: 2, column: 1 },
+        { blockIndex: 3, row: 2, column: 0 },
+        { blockIndex: 3, row: 3, column: 0 },
+        { blockIndex: 0, row: 0, column: 0 },
+        { blockIndex: 3, row: 0, column: 2 },
+        { blockIndex: 0, row: 1, column: 0 },
+        { blockIndex: 3, row: 1, column: 2 },
+        { blockIndex: 3, row: 2, column: 2 },
+        { blockIndex: 3, row: 3, column: 2 },
+        { blockIndex: 0, row: 0, column: 1 },
+        { blockIndex: 1, row: 0, column: 1 },
+        { blockIndex: 1, row: 0, column: 2 },
+        { blockIndex: 3, row: 0, column: 1 },
+        { blockIndex: 0, row: 1, column: 1 },
+        { blockIndex: 1, row: 1, column: 1 },
+    ];
 
     const calculated = sut
     .getPassengersSeatsArrangement(30, [[3, 2], [4, 3], [2, 3], [3, 4]]);
     
-    Object.entries(expected)
-    .forEach(([passengerNumber, { blockIndex, row, column }]) => {
+    expected.forEach(({ blockIndex, row, column }, i) => {
     
-        expect(calculated.hasOwnProperty(passengerNumber))
+        expect(calculated.hasOwnProperty(i))
         .toEqual(true);
 
-        const calculatedSeatLocation = calculated[parseInt(passengerNumber, 10)];
+        const [
+            calcultedBlockIndex,
+            calcultedRow,
+            calcultedColumn,
+        ] = calculated[i];
     
         expect(blockIndex)
-        .toEqual(calculatedSeatLocation.blockIndex);
+        .toEqual(calcultedBlockIndex);
     
         expect(row)
-        .toEqual(calculatedSeatLocation.row);
+        .toEqual(calcultedRow);
     
         expect(column)
-        .toEqual(calculatedSeatLocation.column);
+        .toEqual(calcultedColumn);
 
     });
     
@@ -63,38 +66,41 @@ test('test-1', () => {
 
 test('test-2', () => {
 
-    const expected = {
-        1: { blockIndex: 0, row: 0, column: 0 },
-        2: { blockIndex: 1, row: 0, column: 0 },
-        3: { blockIndex: 1, row: 0, column: 1 },
-        4: { blockIndex: 2, row: 0, column: 0 },
-        5: { blockIndex: 0, row: 1, column: 0 },
-        6: { blockIndex: 1, row: 1, column: 0 },
-        7: { blockIndex: 1, row: 1, column: 1 },
-        8: { blockIndex: 2, row: 1, column: 0 },
-        9: { blockIndex: 1, row: 2, column: 0 },
-        10: { blockIndex: 1, row: 2, column: 1 },
-    }
+    const expected = [
+        { blockIndex: 0, row: 0, column: 0 },
+        { blockIndex: 1, row: 0, column: 0 },
+        { blockIndex: 1, row: 0, column: 1 },
+        { blockIndex: 2, row: 0, column: 0 },
+        { blockIndex: 0, row: 1, column: 0 },
+        { blockIndex: 1, row: 1, column: 0 },
+        { blockIndex: 1, row: 1, column: 1 },
+        { blockIndex: 2, row: 1, column: 0 },
+        { blockIndex: 1, row: 2, column: 0 },
+        { blockIndex: 1, row: 2, column: 1 },
+    ];
     
     const calculated = sut
     .getPassengersSeatsArrangement(10, [[1, 2], [2, 5], [1, 4]]);
     
-    Object.entries(expected)
-    .forEach(([passengerNumber, { blockIndex, row, column }]) => {
+    expected.forEach(({ blockIndex, row, column }, i) => {
     
-        expect(calculated.hasOwnProperty(passengerNumber))
+        expect(calculated.hasOwnProperty(i))
         .toEqual(true);
 
-        const calculatedSeatLocation = calculated[parseInt(passengerNumber, 10)];
+        const [
+            calcultedBlockIndex,
+            calcultedRow,
+            calcultedColumn,
+        ] = calculated[i];
     
         expect(blockIndex)
-        .toEqual(calculatedSeatLocation.blockIndex);
+        .toEqual(calcultedBlockIndex);
     
         expect(row)
-        .toEqual(calculatedSeatLocation.row);
+        .toEqual(calcultedRow);
     
         expect(column)
-        .toEqual(calculatedSeatLocation.column);
+        .toEqual(calcultedColumn);
 
     });
     
@@ -102,38 +108,41 @@ test('test-2', () => {
 
 test('test-3', () => {
 
-    const expected = {
-        1: { blockIndex: 0, row: 0, column: 0 },
-        2: { blockIndex: 1, row: 0, column: 0 },
-        3: { blockIndex: 2, row: 0, column: 0 },
-        4: { blockIndex: 1, row: 1, column: 0 },
-        5: { blockIndex: 1, row: 2, column: 0 },
-        6: { blockIndex: 1, row: 3, column: 0 },
-        7: { blockIndex: 1, row: 4, column: 0 },
-        8: { blockIndex: 2, row: 0, column: 4 },
-        9: { blockIndex: 2, row: 0, column: 1 },
-        10: { blockIndex: 2, row: 0, column: 2 },
-    };
+    const expected = [
+        { blockIndex: 0, row: 0, column: 0 },
+        { blockIndex: 1, row: 0, column: 0 },
+        { blockIndex: 2, row: 0, column: 0 },
+        { blockIndex: 1, row: 1, column: 0 },
+        { blockIndex: 1, row: 2, column: 0 },
+        { blockIndex: 1, row: 3, column: 0 },
+        { blockIndex: 1, row: 4, column: 0 },
+        { blockIndex: 2, row: 0, column: 4 },
+        { blockIndex: 2, row: 0, column: 1 },
+        { blockIndex: 2, row: 0, column: 2 },
+    ];
 
     const calculated = sut
     .getPassengersSeatsArrangement(10, [[1, 1], [1, 5], [5, 1]]);
-
-    Object.entries(expected)
-    .forEach(([passengerNumber, { blockIndex, row, column }]) => {
     
-        expect(calculated.hasOwnProperty(passengerNumber))
+    expected.forEach(({ blockIndex, row, column }, i) => {
+    
+        expect(calculated.hasOwnProperty(i))
         .toEqual(true);
 
-        const calculatedSeatLocation = calculated[parseInt(passengerNumber, 10)];
+        const [
+            calcultedBlockIndex,
+            calcultedRow,
+            calcultedColumn,
+        ] = calculated[i];
     
         expect(blockIndex)
-        .toEqual(calculatedSeatLocation.blockIndex);
+        .toEqual(calcultedBlockIndex);
     
         expect(row)
-        .toEqual(calculatedSeatLocation.row);
+        .toEqual(calcultedRow);
     
         expect(column)
-        .toEqual(calculatedSeatLocation.column);
+        .toEqual(calcultedColumn);
 
     });
 
